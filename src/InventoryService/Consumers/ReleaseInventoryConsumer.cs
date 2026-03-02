d30592d3ce48c28ea1ca2c52f4d19998f26cec0f
devjuliovilla <122956628+devjuliovilla@users.noreply.github.com>
2026-03-02 11:46:31 -0600

    Created inventory consumers

---
+++ src/InventoryService/Consumers/ReleaseInventoryConsumer.cs
@@ -0,0 +1,22 @@
+﻿using Contracts.Commands;
+using MassTransit;
+
+namespace InventoryService.Consumers
+{
+    public class ReleaseInventoryConsumer(ILogger<ReleaseInventoryConsumer> logger) : IConsumer<ReleaseInventory>
+    {
+        private readonly ILogger<ReleaseInventoryConsumer> _logger = logger;
+
+        public  async Task Consume(ConsumeContext<ReleaseInventory> context)
+        {
+            var msg = context.Message;
+
+            _logger.LogInformation(
+                $"ReleaseInventory received CorrelationId={msg.CorrelationId} OrderId={msg.OrderId} ProductId={msg.ProductId} Qty={msg.Quantity}");
+
+            await Task.Delay(2000);
+
+
+        }
+    }
+}
---
+++ src/InventoryService/Consumers/ReserveInventoryConsumer.cs
@@ -0,0 +1,38 @@
+﻿using Contracts.Commands;
+using Contracts.Events;
+using MassTransit;
+using Microsoft.Extensions.Logging;
+
+namespace InventoryService.Consumers
+{
+    public class ReserveInventoryConsumer(ILogger<ReserveInventoryConsumer> logger) : IConsumer<ReserveInventory>
+    {
+        private readonly ILogger<ReserveInventoryConsumer> _logger = logger;
+
+        public async Task Consume(ConsumeContext<ReserveInventory> context)
+        {
+            var msg = context.Message;
+
+            _logger.LogInformation(
+                $"ReserveInventory received CorrelationId={msg.CorrelationId} OrderId={msg.OrderId} ProductId={msg.ProductId} Qty={msg.Quantity}");
+
+            var canReserve = msg.Quantity > 0;
+
+            if (!canReserve)
+            {
+                await context.Publish(new InventoryRejected(
+                    msg.CorrelationId,
+                    msg.OrderId,
+                    "Invalid quantity"));
+
+                return;
+            }
+
+            await Task.Delay(2000);
+
+            await context.Publish(new InventoryReserved(
+                msg.CorrelationId,
+                msg.OrderId));
+            ;        }
+    }
+}
