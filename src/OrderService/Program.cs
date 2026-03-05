ed001cea727dc713e2dbf8914a7a83e5a32d2072
devjuliovilla <122956628+devjuliovilla@users.noreply.github.com>
2026-03-05 16:37:54 -0600

    Add retries on db.Migrate

--- src/OrderService/Program.cs
+++ src/OrderService/Program.cs
@@ -1,3 +1,4 @@
+using Contracts.Events;
 using MassTransit;
 using Microsoft.EntityFrameworkCore;
 using OrderService.Domain.Sagas;
@@ -53,11 +54,51 @@ app.MapControllers();
 using (var scope = app.Services.CreateScope())
 {
     var db = scope.ServiceProvider.GetRequiredService<SagaDbContext>();
-    db.Database.Migrate();
+
+    int retries = 10;
+
+    while (retries > 0)
+    {
+        try
+        {
+            Console.WriteLine("Applying database migrations...");
+            db.Database.Migrate();
+            Console.WriteLine("Database migrations applied successfully.");
+            break;
+        }
+        catch (Exception ex)
+        {
+            retries--;
+
+            Console.WriteLine($"Database not ready yet. Retrying in 3 seconds... Attempts left: {retries}");
+            Console.WriteLine(ex.Message);
+
+            Thread.Sleep(3000);
+        }
+    }
 }
 
+// Endpoints
+#region Endpoints
 app.MapGet("/", () => "OrderService running");
 
-app.UseHttpsRedirection();
+app.MapPost("/orders", async (IPublishEndpoint publishEndpoint) =>
+{
+    var correlationId = Guid.NewGuid();
+    var orderId = Guid.NewGuid();
+
+    await publishEndpoint.Publish(new OrderSubmitted(
+        correlationId,
+        orderId,
+        "Product 1",
+        25));
+
+    return Results.Accepted($"/orders/{orderId}", new
+    {
+        Message = "Order creation started",
+        OrderId = orderId
+    });
+});
+#endregion
 
 app.Run();
